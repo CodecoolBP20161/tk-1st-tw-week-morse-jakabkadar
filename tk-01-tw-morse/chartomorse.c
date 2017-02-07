2a7915da0a12703ef5a52c78a88f82f849571fa3
arvisura87 <jakab.tamas@me.com>
2017-02-07 15:09:25 +0100

    char to morse done

--- tk-01-tw-morse/chartomorse.c
+++ tk-01-tw-morse/chartomorse.c
@@ -1,4 +1,5 @@
 #include "selfprot.h"
+#include <string.h>
 
 /*
  * Function: CharToMorse
@@ -13,4 +14,24 @@
  * returns: the index of the last converted character of the input
  */
 int CharToMorse(char input[], char *output) {
+
+	int i;
+	int r;
+
+	for (i=0; i<=strlen(input); i++){
+
+		if (i<strlen(input)-1){
+			strcat(output, CHAR_TO_MORSE_ALPHA[((int)input[i])-65]);
+			strcat(output, "/");
+
+		} else if (i<strlen(input)){
+			strcat(output, CHAR_TO_MORSE_ALPHA[((int)input[i])-65]);
+
+		} else {
+			strcat(output, "\n");
+			r=input[i-1];
+		}
+	}
+
+	return r;
 }
