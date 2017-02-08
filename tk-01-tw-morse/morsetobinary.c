b45c86a277d2a767db705b1324c39806673d2714
arvisura87 <jakab.tamas@me.com>
2017-02-08 15:02:01 +0100

    morse to binary function skeleton

--- tk-01-tw-morse/morsetobinary.c
+++ tk-01-tw-morse/morsetobinary.c
@@ -1,4 +1,5 @@
 #include "selfprot.h"
+#include <string.h>
 
 /*
  * Function: MorseToBinary
@@ -11,4 +12,27 @@
  * returns: the number of used bits for morse
  */
 int MorseToBinary(char input[], char output[]) {
+
+	int i;
+	char conv[15]="";
+
+	for (i=0; i<=strlen(input); i++){
+		switch(input[i]) {
+		      case '.' :
+		    	 strcat(conv, "1");
+		         break;
+		      case '-' :
+		    	 strcat(conv, "111");
+		    	 break;
+		      case ' ' :
+		    	 strcat(conv, "0");
+		         break;
+		      case '/' :
+		    	 strcat(conv, "000");
+		         break;
+		      case '\t' :
+		    	 strcat(conv, "0000000");
+		         break;
+		   }
+	}
 }
